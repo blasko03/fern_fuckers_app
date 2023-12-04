0b88525bc468c43082b03bb35f1b866c2377f7c2
Daniel Blasina <daniel.blasina@modefinance.com>
2023-12-04 18:50:39 +0100

    added frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,26 @@
+import { type ReactElement } from 'react'
+
+export default function Home (): ReactElement {
+  return (
+    <main>
+      <table style={{ width: '100%' }}>
+        <thead>
+          <th>Ime</th>
+          <th>Točka</th>
+          <th>Leg</th>
+          <th>Leg</th>
+          <th>Točka</th>
+          <th>Ime</th>
+        </thead>
+        <tbody>
+          <td><select><option>aaaaaaaa</option></select></td>
+          <td></td>
+          <td><input value="" type="number" /></td>
+          <td><input value="" type="number" /></td>
+          <td></td>
+          <td><select><option>aaaaaaaa</option></select></td>
+        </tbody>
+      </table>
+    </main>
+  )
+}
