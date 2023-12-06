b4dd6ffe95b95c0e4c6546d63ea54c5b72dd61f9
Daniel Blasina <blzk100@gmail.com>
2023-12-06 08:21:23 +0100

    implementing form for creating championship

--- frontend/src/app/championship/create/page.tsx
+++ frontend/src/app/championship/create/page.tsx
@@ -1,11 +1,24 @@
 'use client'
-import { type ReactElement } from 'react'
+import { useState, type ReactElement, type ChangeEventHandler } from 'react'
+
+interface Team {
+  name?: string
+  players: string[]
+}
 
 export default function Home (): ReactElement {
   const handleSubmit = (event: any): void => {
     console.log(event.target.value)
     event.preventDefault()
   }
+
+  const checkobxSelect: ChangeEventHandler<HTMLInputElement> = (event) => {
+    console.log(event.target.defaultValue)
+    setFormState(f => ({ ...f, players: f.players }))
+  }
+
+  const [formState, setFormState] = useState<Team>({ players: [] })
+
   return (
     <main>
       <form onSubmit={handleSubmit}>
@@ -13,14 +26,14 @@ export default function Home (): ReactElement {
             Championship name
         </div>
         <div>
-           <input type='text' />
+           <input type='text' value={formState.name} />
         </div>
 
         <div>
             Members
         </div>
         <div>
-           <div><input type='checkbox' name='teams[]' value='team1' /> Team1</div>
+           <div><input type='checkbox' name='teams[]' value='team1' onChange={checkobxSelect} /> Team1</div>
            <div><input type='checkbox' name='teams[]' value='team2' /> Team2</div>
            <div><input type='checkbox' name='teams[]' value='team3' /> Team3</div>
         </div>
