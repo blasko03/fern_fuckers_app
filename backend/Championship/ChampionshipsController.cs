f57a42989b24986c29ca7f9870b0cb341492de53
Daniel Blasina <blzk100@gmail.com>
2023-12-10 00:34:13 +0100

    feat: reorganized code

---
+++ backend/Championship/ChampionshipsController.cs
@@ -0,0 +1,50 @@
+﻿using FernFuckersAppBackend.Controllers.Params;
+using FernFuckersAppBackend.Controllers.Responses;
+using FernFuckersAppBackend.Models;
+using FernFuckersAppBackend.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FernFuckersAppBackend.Controllers;
+
+[Route("api/championships")]
+[ApiController]
+
+public class ChampionshipsController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<ChampionshipResponse>> Get(ApplicationDbContext context)
+    {
+        var teams = context.Teams;
+        return await context.Championships.Include(e => e.Teams)
+                                          .ThenInclude(team => team.Players)
+                                          .Include(e => e.Matches)
+                                          .ThenInclude(match => match.Teams)
+                                          .Include(e => e.Matches)
+                                          .ThenInclude(match => match.Sets)
+                                          .ThenInclude(set => set.Players)
+                                          .Select(x => (ChampionshipResponse)x).ToListAsync();
+    }
+
+    [HttpPost]
+    public async Task<Results<BadRequest, Ok<ChampionshipResponse>>> Create([FromBody] ChampionshipParams championship, ApplicationDbContext context)
+    {
+        return await ServiceCaller.Call<ChampionshipResponse>(() => CreateChampionshipService.Call(context, championship));
+    }
+    /*
+    [HttpGet("events")]
+    public async Task Events()
+    {
+        var response = Response;
+        response.Headers.Append("Content-Type", "text/event-stream");
+
+        for (var i = 0; true; ++i)
+        {
+            await response.WriteAsync($"data: Controller {i} at {DateTime.Now}\r\r");
+            await response.Body.FlushAsync();
+            await Task.Delay(5 * 1000);
+        }
+    }
+    */
+}
---
+++ backend/Player/PlayersController.cs
@@ -0,0 +1,27 @@
+﻿using FernFuckersAppBackend.Controllers.Params;
+using FernFuckersAppBackend.Controllers.Responses;
+using FernFuckersAppBackend.Models;
+using FernFuckersAppBackend.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FernFuckersAppBackend.Controllers;
+
+[Route("api/players")]
+[ApiController]
+public class PlayersController : ControllerBase
+{
+    [HttpGet]
+    public async Task<List<PlayerResponse>> Get(ApplicationDbContext context)
+    {
+        return await context.Players.Select(x => (PlayerResponse)x).ToListAsync();
+    }
+
+    [HttpPost]
+
+    public async Task<Results<BadRequest, Ok<PlayerResponse>>> Create([FromBody] PlayerParams player, ApplicationDbContext context)
+    {
+        return await ServiceCaller.Call<PlayerResponse>(() => CreatePlayerService.Call(context, player));
+    }
+}
