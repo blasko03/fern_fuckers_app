bb33b2a4506ab360d0d4377f92abf78b9d8351d6
Daniel Blasina <blzk100@gmail.com>
2023-12-10 15:44:25 +0100

    feat: removed throw controller and moved thows to won leg

--- backend/Leg/WonLegParams.cs
+++ backend/Leg/WonLegParams.cs
@@ -3,4 +3,5 @@
 public class WonLegParams
 {
     public required Guid TeamId { get; set; }
+    public List<ThrowParam> Throws { get; set; } = [];
 }
---
+++ backend/Throw/ThrowParam.cs
@@ -0,0 +1,8 @@
+﻿namespace FernFuckersAppBackend.Controllers.Params;
+
+public class ThrowParam
+{
+    public required Guid TeamId { get; set; }
+    public required int Score { get; set; }
+    public required int NumberOfDarts { get; set; }
+}
