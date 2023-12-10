082ba013dbfb9c09052ab05ddf36a4107c22878e
Daniel Blasina <blzk100@gmail.com>
2023-12-10 15:27:44 +0100

    fix: pileine

--- backend/Leg/LegsController.cs
+++ backend/Leg/LegsController.cs
@@ -15,6 +15,7 @@ public class LegsController : ControllerBase
     [HttpPost("{id}/wonLeg")]
     public async Task<Results<BadRequest, Ok<WonLegResponse>>> WonLeg([FromBody] WonLegParams param, Guid id, ApplicationDbContext context)
     {
+        Console.WriteLine(id);
         return await ServiceCaller.Call<WonLegResponse>(() => WonLegService.Call(context, param));
     }
 }
