a71f4dc6192bdab4f6a1d83d9e93234407d361e7
Daniel Blasina <blzk100@gmail.com>
2023-12-27 00:44:48 +0100

    feat: implemented stats page

--- frontend/src/app/championships/[id]/stats/page.tsx
+++ frontend/src/app/championships/[id]/stats/page.tsx
@@ -1,11 +1,32 @@
 'use client'
 
-import { type ReactElement } from 'react'
+import { type Team } from '@/interfaces/Team'
+import { serverRequest } from '@/utils/serverData'
+import { useEffect, type ReactElement, useState } from 'react'
 
-export default function Stats (): ReactElement {
+interface Props {
+  params: {
+    id: string
+  }
+}
+
+interface TeamPoints {
+  team: Team
+  points: number
+}
+
+export default function Stats ({ params: { id } }: Props): ReactElement {
+  const [ranking, setRanking] = useState<TeamPoints[]>()
+  const getData = async (id: string): Promise<void> => {
+    setRanking(((await serverRequest<TeamPoints[]>(`/api/championships/${id}/stats`))))
+  }
+
+  useEffect(() => {
+    getData(id).catch(error => { console.log(error) })
+  }, [id])
   return (
     <main>
-        <div>Tema1: points</div>
+      {ranking?.map(tp => <div key={tp.team.id}>{tp.team.name}: {tp.points}</div>)}
     </main>
   )
 }
