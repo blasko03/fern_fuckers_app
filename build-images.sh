fe80216f1439d1791cd6849aa633a2edfb9e8d9a
Daniel Blasina <daniel.blasina@modefinance.com>
2024-01-11 14:01:45 +0100

    changed dockerfile

--- build-images.sh
+++ build-images.sh
@@ -2,7 +2,7 @@ docker buildx build . -f ./backend/.docker/Dockerfile \
                -t blasko/fern_fuckers_app_backend:1.0.1 \
                --platform linux/arm64 \
                --push
-docker buildx build . -f ./frontend/.docker/Dockerfile \
-               -t blasko/fern_fuckers_app_frontend:1.0.1 \
+docker buildx build frontend -f ./frontend/.docker/Dockerfile \
+               -t blasko/fern_fuckers_app_frontend:1.0.3 \
                --platform linux/arm64 \
                --push
